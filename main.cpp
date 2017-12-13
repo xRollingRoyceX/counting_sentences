73d8f09580eb48176de89277f7bab2df3c6ca761
Sean <jesky.sean@outlook.com>
2017-12-13 10:08:54 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 //#include<cctype>
 //self-start-project 0x12:
 //count each sentence that is as correct as possible.
-const string::size_type sentence_periods(string::size_type len_count, const string &s) {
+const string::size_type sentences(string::size_type len_count, const string &s) {
     len_count = 0 + 1; //to stop the off by one counting for easier reading(... 0 + 1 ...) of results
     for (auto i = 0; i != s.size() && !s.empty(); ++i) {
         if (s[i] == '.' && isupper(s[i + 2])) {
@@ -18,5 +18,5 @@ int main()
             " And this is the last sentence. Hello World, this is a program!.";
     string::size_type n = 0;
 
-    cout << sentence_periods(n, s);
+    cout << sentences(n, s);
 }
