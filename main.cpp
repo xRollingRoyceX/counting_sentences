745f0e550cc6814f29a004397df0a011facacba8
Sean <jesky.sean@outlook.com>
2017-12-13 09:57:10 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <inclds>
+#include<cctype>
+//self-start-project 0x12:
+//count each sentence that is as correct as possible.
+const string::size_type sentence_periods(string::size_type len_count, const string &s) {
+    int cnt = 0 + 1;
+    for (auto i = 0; i != s.size() && !s.empty(); ++i) {
+        if (s[i] == '.' && isupper(s[i + 2])) {
+            cnt += 1;
+        }
+    }
+    return cnt;
+}
+int main()
+{
+    //gives result of: three sentences.
+    string s = "This is a sentence. This part is a sentence,"
+            " And this is the last sentence. Hello World, this is a program!.";
+    string::size_type n = 0;
+
+    cout << sentence_periods(n, s);
+}
